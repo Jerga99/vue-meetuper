b440364c404aaa949eea5d6e1dcada2c44847c11
Filip Jerga <jerga99@gmail.com>
2019-02-19 11:53:17 +0000

    32 Send a request to login an user

--- src/pages/PageLogin.vue
+++ src/pages/PageLogin.vue
@@ -85,6 +85,8 @@
       login () {
         this.$v.form.$touch()
         this.$store.dispatch('auth/loginWithEmailAndPassword', this.form)
+          .then(() => this.$router.push('/'))
+          .catch((err) => console.log(err))
       }
     }
   }
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,14 +3,23 @@ import axios from 'axios'
 export default {
   namespaced: true,
   state: {
-
+    user: {}
   },
   actions: {
-    loginWithEmailAndPassword (context, userData) {
-      console.log(userData)
+    loginWithEmailAndPassword ({commit}, userData) {
+      return axios.post('/api/v1/users/login', userData)
+        .then(res => {
+          const user = res.data
+          commit('setAuthUser', user)
+        })
     },
     registerUser (context, userData) {
       return axios.post('/api/v1/users/register', userData)
+    }
+  },
+  mutations: {
+    setAuthUser (state, user) {
+      return state.user = user
     }
   }
 }
