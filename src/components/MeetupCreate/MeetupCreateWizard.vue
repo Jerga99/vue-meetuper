945719c8fd453c35c38495d4492d7e69c8dfa1b3
Filip Jerga <jerga99@gmail.com>
2019-02-21 18:53:19 +0000

    48 Separate Meetup Create into Multiple Steps

--- src/components/MeetupCreate/MeetupCreateWizard.vue
+++ src/components/MeetupCreate/MeetupCreateWizard.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="meetup-create-form">
     <div class="current-step is-pulled-right">
-      1 of 4
+      {{currentStep}} of {{allStepsCount}}
     </div>
     <!-- Form Steps -->
-    <MeetupLocation />
-    <MeetupDetail />
-    <MeetupDescription />
-    <MeetupConfirmation />
+    <MeetupLocation v-if="currentStep === 1" />
+    <MeetupDetail v-if="currentStep === 2" />
+    <MeetupDescription v-if="currentStep === 3" />
+    <MeetupConfirmation v-if="currentStep === 4" />
 
-    <progress class="progress" :value="100" max="100">100%</progress>
+    <progress class="progress"
+              :value="currentProgress"
+               max="100">{{currentProgress}}%</progress>
     <div class="controll-btns m-b-md">
-      <button class="button is-primary m-r-sm">Back</button>
-      <button class="button is-primary">Next</button>
-      <!-- Confirm Data -->
-      <!-- <button v-else
-              class="button is-primary">Confirm</button> -->
+      <button v-if="currentStep !== 1"
+              @click="moveToPreviousStep"
+              class="button is-primary m-r-sm">Back</button>
+      <button v-if="currentStep !== allStepsCount"
+              @click="moveToNextStep"
+              class="button is-primary">Next</button>
+      <button v-else
+              class="button is-primary">Confirm</button>
     </div>
     <!-- Just To See Data in the Form -->
     <pre><code>{{form}}</code></pre>
@@ -36,6 +41,8 @@
     },
     data () {
       return {
+        currentStep: 1,
+        allStepsCount: 4,
         form: {
           location: null,
           title: null,
@@ -47,6 +54,19 @@
           timeTo: null,
           timeFrom: null
         }
+      }
+    },
+    computed: {
+      currentProgress () {
+        return (100 / this.allStepsCount) * this.currentStep
+      }
+    },
+    methods: {
+      moveToNextStep () {
+        this.currentStep++
+      },
+      moveToPreviousStep () {
+        this.currentStep--
       }
     }
   }
