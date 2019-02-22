5ea575154d343834806057ba7380e8262d1fe4ab
Filip Jerga <jerga99@gmail.com>
2019-02-22 17:41:05 +0000

    60 Use Computed Properties in Template

--- src/pages/PageMeetupDetail.vue
+++ src/pages/PageMeetupDetail.vue
@@ -26,7 +26,7 @@
         </div>
         <div class="is-pulled-right">
           <!-- We will handle this later (: -->
-          <button class="button is-danger">Leave Group</button>
+          <button v-if="isMember" class="button is-danger">Leave Meetup</button>
         </div>
       </div>
     </section>
@@ -81,11 +81,10 @@
             <div class="content is-medium">
               <h3 class="title is-3">About the Meetup</h3>
               <p>{{meetup.description}}</p>
-              <!-- Join Meetup, We will handle it later (: -->
-              <button class="button is-primary">Join In</button>
-              <!-- Not logged In Case, handle it later (: -->
-              <!-- <button :disabled="true"
-                      class="button is-warning">You need authenticate in order to join</button> -->
+              <button v-if="canJoin" class="button is-primary">Join In</button>
+              <button v-if="!isAuthenticated"
+                      :disabled="true"
+                      class="button is-warning">You need authenticate in order to join</button>
             </div>
             <!-- Thread List START -->
             <div class="content is-medium">
