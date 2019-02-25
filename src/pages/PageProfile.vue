e14f4e21cfab3d660e8e7dab813165ddda85d656
Filip Jerga <jerga99@gmail.com>
2019-02-25 18:59:21 +0000

    86 Display Stats Data

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -6,36 +6,39 @@
           <div class="column is-2">
             <figure class="image  header-icon user-profile-image">
               <!-- TODO: Get user avatar here -->
-              <img class="is-rounded" src=""/>
+              <img class="is-rounded" :src="user.avatar"/>
             </figure>
           </div>
           <div class="column is-4-tablet is-10-mobile name">
             <p>
               <!-- TODO: Display user name here -->
-              <span class="title is-bold">John Example</span>
+              <span class="title is-bold">{{user.name}}</span>
               <br/>
               <!-- Here will be user update functionality -->
               <button class="button is-primary is-outlined m-t-sm">Update Info</button>
               <br/>
             </p>
-            <!-- TODO: User Info Here if any -->
+            <!-- TODO: User info Here if any -->
             <p class="tagline">
-              I am very productive and good programmer
+              {{user.info}}
             </p>
           </div>
           <!-- TODO: Set Active Tab to 'meetups' and class to 'isActive' -->
           <div class="stats-tab column is-2-tablet is-4-mobile has-text-centered">
-            <p class="stat-val">2</p>
+            <!-- TODO: Display Meetups count -->
+            <p class="stat-val">{{meetupsCount}}</p>
             <p class="stat-key">Meetups</p>
           </div>
           <!-- TODO: Set Active Tab to 'threads' and class to 'isActive' -->
           <div class="stats-tab column is-2-tablet is-4-mobile has-text-centered">
-            <p class="stat-val">4</p>
+            <!-- TODO: Display Threads count -->
+            <p class="stat-val">{{threadsCount}}</p>
             <p class="stat-key">Threads</p>
           </div>
           <!-- TODO: Set Active Tab to 'posts' and class to 'isActive' -->
           <div class="stats-tab column is-2-tablet is-4-mobile has-text-centered">
-            <p class="stat-val">3</p>
+            <!-- TODO: Display Posts count -->
+            <p class="stat-val">{{postsCount}}</p>
             <p class="stat-key">Posts</p>
           </div>
         </div>
@@ -43,28 +46,30 @@
       <!-- TODO: Display this div when activeTab === 'meetups' -->
       <div class="columns is-mobile is-multiline">
         <!-- TODO: Iterate over meetups -->
-        <div class="column is-3-tablet is-6-mobile">
-          <!-- THREADS -->
+        <div v-for="meetup in meetups" :key="meetup._id" class="column is-3-tablet is-6-mobile">
+          <!-- MEETUPS -->
           <div class="card">
             <div class="card-image">
               <figure class="image is-4by3">
                 <!-- TODO: Display Meetup Image -->
-                <img src=""/>
+                <img :src="meetup.image"/>
               </figure>
             </div>
             <div class="card-content">
               <div class="media">
                 <div class="media-content">
                   <!-- TODO: Display Meetup title -->
-                  <p class="title is-4">Nice Meetup</p>
+                  <p class="title is-4">{{meetup.title}}</p>
                   <!-- TODO: Display Category name -->
-                  <p class="subtitle is-6"><span class="tag is-dark subtitle">Sport</span></p>
+                  <p class="subtitle is-6">
+                    <span class="tag is-dark subtitle">{{meetup.category.name}}</span>
+                  </p>
                 </div>
               </div>
               <div class="content">
                 <!-- TODO: Display Meetup shortInfo -->
                 <p>
-                  Some short info
+                  {{meetup.shortInfo}}
                 </p>
               </div>
             </div>
@@ -79,14 +84,14 @@
       <!-- TODO: Display this div when activeTab === 'threads' -->
       <div class="columns is-mobile is-multiline">
         <!-- TODO: Iterate over threads -->
-        <div class="column is-3-tablet is-6-mobile">
+        <div v-for="thread in threads" :key="thread._id" class="column is-3-tablet is-6-mobile">
           <!-- THREADS -->
           <div class="card">
             <div class="card-content">
               <div class="media">
                 <div class="media-content">
                   <!-- TODO: Display thread thread title -->
-                  <p class="title is-4">Some title</p>
+                  <p class="title is-4">{{thread.title}}</p>
                 </div>
               </div>
             </div>
@@ -101,14 +106,14 @@
       <!-- TODO: Display this div when activeTab === 'posts' -->
       <div class="columns is-mobile is-multiline">
         <!-- TODO: Iterate over posts -->
-        <div class="column is-3-tablet is-6-mobile">
-          <!-- THREADS -->
+        <div v-for="post in posts" :key="post._id" class="column is-3-tablet is-6-mobile">
+          <!-- POSTS -->
           <div class="card">
             <div class="card-content">
               <div class="media">
                 <div class="media-content">
                   <!-- TODO: Display post text -->
-                  <p class="title is-4">Some Text</p>
+                  <p class="title is-4">{{post.text}}</p>
                 </div>
               </div>
             </div>
@@ -125,7 +130,19 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   export default {
+    computed: {
+      ...mapState({
+        user: state => state.auth.user,
+        meetups: state => state.stats.meetups.data,
+        threads: state => state.stats.threads.data,
+        posts: state => state.stats.posts.data,
+        meetupsCount: state => state.stats.meetups.count,
+        threadsCount: state => state.stats.threads.count,
+        postsCount: state => state.stats.posts.count
+      })
+    },
     created () {
       this.$store.dispatch('stats/fetchUserStats')
         .then(stats => console.log(stats))
