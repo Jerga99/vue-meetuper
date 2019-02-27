6e7ef1646299e2535350bfc102d57e089fd4c4e0
Filip Jerga <jerga99@gmail.com>
2019-02-27 02:56:03 +0000

    95 Display Location in Meetup Create Page

--- src/components/MeetupCreate/MeetupLocation.vue
+++ src/components/MeetupCreate/MeetupLocation.vue
@@ -2,9 +2,11 @@
   <div>
     <h1 class="title m-b-sm">What's your new Meetup location?</h1>
     <div class="m-b-lg">
-      <span class="subtitle">New York, US</span>
-      <a>(change location)</a>
-      <input @input="emitFormData"
+      <span v-if="ipLocation && !wantChangeLocation" class="subtitle">{{ipLocation}}</span>
+      <a v-if="ipLocation && !wantChangeLocation" @click="toggleLocation">(change location)</a>
+      <a v-if="ipLocation && wantChangeLocation" @click="toggleLocation">(Set Default location)</a>
+      <input v-if="!ipLocation || wantChangeLocation"
+             @input="emitFormData"
              @blur="$v.form.location.$touch()"
              v-model="form.location"
              type="text"
@@ -21,6 +23,7 @@
   export default {
     data () {
       return {
+         wantChangeLocation: false,
          form: {
            location: null
         }
@@ -31,9 +34,28 @@
         location: {required}
       }
     },
+    computed: {
+      ipLocation () {
+        return this.$store.getters['meta/location']
+      }
+    },
+    created () {
+      if (this.ipLocation) {
+        this.form.location = this.ipLocation
+        this.emitFormData()
+      }
+    },
     methods: {
       emitFormData () {
         this.$emit('stepUpdated', {data: this.form, isValid: !this.$v.$invalid})
+      },
+      toggleLocation () {
+        if (this.ipLocation) {
+          this.form.location = this.ipLocation
+          this.emitFormData()
+        }
+
+        this.wantChangeLocation = !this.wantChangeLocation
       }
     }
   }
